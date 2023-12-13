3d2df557942eb9e4aa1e8f7843ade925a46e08f4
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2023-12-13 10:38:53 -0500

    p25: Implement first-try working solution. :)

--- 2023/25/reflect.pl
+++ 2023/25/reflect.pl
@@ -11,13 +11,14 @@ use autodie;
 use experimental 'for_list';
 
 use Math::BigInt;
-use List::Util qw(min max sum reduce none);
+use List::Util qw(min max sum reduce none first);
 use Mojo::JSON qw(j);
 use POSIX qw(ceil);
 use Term::ANSIColor qw(:constants);
 
 # Config
-use constant G_DEBUG_INPUT => 1;
+use constant G_DEBUG_INPUT => 0;
+use constant G_DEBUG_REFLECTIONS => 0;
 
 my $input_name = 'input';
 $" = ', '; # For arrays interpolated into strings
@@ -52,7 +53,6 @@ my $finalize_grid = sub {
     $grid_line  = 0;
 };
 
-say scalar @lines, " lines";
 for (@lines) {
     if ($_) {
         push @$cur_grid, $_;
@@ -83,5 +83,83 @@ if (G_DEBUG_INPUT) {
     say "Read in ", scalar @grids, " arrays";
 }
 
+my $sum = 0;
+for (my $i = 0; $i < scalar @grids; $i++) {
+    my $horiz_match =
+        first { check_horiz_reflection_at($grids[$i], $_) }
+        find_horiz_reflection($grids[$i]);
+
+    if ($horiz_match) {
+        $sum += 100 * $horiz_match;
+        next;
+    }
+
+    my $vert_match =
+        first { check_horiz_reflection_at($grids_t[$i], $_) }
+        find_horiz_reflection($grids_t[$i]);
+
+    if ($vert_match) {
+        $sum += $vert_match;
+        next;
+    }
+
+    # if we get here something is wrong
+    die "No reflection found in grid $i!";
+}
+
+say $sum;
+
 # Aux subs
 
+sub find_horiz_reflection($grid_ref)
+{
+    my @lines = $grid_ref->@*;
+    my @idx_candidates;
+    my $line_idx = 1;
+    my $last_line = shift @lines;
+
+    while(my $line = shift @lines) {
+        if ($line eq $last_line) {
+            push @idx_candidates, $line_idx;
+        }
+
+        $last_line = $line;
+        $line_idx++;
+    }
+
+    return @idx_candidates;
+}
+
+# reflects the grid about the horizontal axis just after line $idx, and
+# returns true if the grid was a true reflection at that point
+sub check_horiz_reflection_at($grid_ref, $idx)
+{
+    my $num_lines = @$grid_ref;
+    my $top = 0;
+    my $bottom = $num_lines - 1;
+
+    if (($num_lines - $idx) == $idx) {
+        # perfect split, don't change lines
+    } elsif (($num_lines - $idx) < $idx) {
+        # near bottom of grid, remove lines at top
+        $top = $num_lines - 2 * ($num_lines - $idx);
+    } else {
+        # near top of grid, remove lines at bottom
+        $bottom = 2 * $idx - 1;
+    }
+
+    my $above_str = join('', @{$grid_ref}[$top..($idx - 1)]);
+    my $below_str = join('', reverse @{$grid_ref}[$idx..$bottom]);
+
+    die "misshappen reflection"
+        unless length($above_str) == length ($below_str);
+
+    if (G_DEBUG_REFLECTIONS) {
+        say "Grid reflected about $idx into:";
+        say "\t[$above_str] and";
+        say "\t[$below_str]";
+        say "Which are ", ($above_str eq $below_str) ? 'equal' : 'not equal';
+    }
+
+    return $above_str eq $below_str;
+}
