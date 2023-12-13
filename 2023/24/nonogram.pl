db58198a6e0011865bcb254592c6665498d35f70
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2023-12-13 14:51:25 -0500

    p24: Do some golfing of old solution to make more clear on method.
    This also adds an LRU cap on the cache size. Doesn't speed things up (if
    anything seems to add a bit of time for me), but does reduce memory
    usage during the run pretty substantially, from ~600K entries to 5-6K.

--- 2023/24/nonogram.pl
+++ 2023/24/nonogram.pl
@@ -9,27 +9,13 @@
 
 use 5.038;
 use autodie;
-use experimental 'for_list';
-
-use Math::BigInt;
-use List::Util qw(min max sum reduce none);
-use Mojo::JSON qw(j);
-use POSIX qw(ceil);
-use Term::ANSIColor qw(:constants);
-
-# Config
-use constant G_DEBUG_INPUT => 0;
-use constant G_DEBUG_ROWS => 0;
-use constant G_DEBUG_MASK => 0;
-use constant G_DEBUG_RESULTS => 0;
-use constant G_DEBUG_CACHE_STATS => 1;
-use constant G_DEBUG_COMBINATORICS => 0;
 
 my $input_name = '../23/input';
 $" = ', '; # For arrays interpolated into strings
 
 # Globals
 my %cache; # for memoization / DP
+my %cache_lru;
 my $cache_hits = 0;
 my $cache_attempts = 0;
 my $cache_inserts = 0;
@@ -49,57 +35,23 @@ my @rows = map {
     [$r, @sums];
 } @lines;
 
-if (G_DEBUG_INPUT) {
-    for (@rows) {
-        my ($row, @checksums) = $_->@*;
-        say "Row $row, @checksums";
-    }
-    say "Read ", scalar @rows, " rows";
-}
-
-my $maxmask = max(map { length $_->[0] } @rows);
-
 my $sum = 0;
 for my $row (@rows) {
     my ($template, @partlens) = $row->@*;
     my $count = count_valid_rows($template, @partlens);
     $sum += $count;
-
-    if (G_DEBUG_RESULTS) {
-        print sprintf("%-${maxmask}s: ", $template), " $count options for ";
-        say "[@partlens]";
-    }
 }
 
 say $sum;
 
-if (G_DEBUG_CACHE_STATS) {
-    say "Cache attempts: $cache_attempts";
-    say "Cache inserts: $cache_inserts";
-    say "Cache size: ", scalar keys %cache;
-    say "Cache hits: $cache_hits";
-    say "Hit rate: ", sprintf("%0.3f", 100.0 * $cache_hits / $cache_attempts);
-}
-
 # Aux subs
-
-sub ll($level, $msg) {
-    return unless G_DEBUG_ROWS;
-    my $indent = (' ') x $level;
-    return if $level > 80;
-    print "$indent$msg -> ";
-}
-
-sub fi($char, $result) {
-    return unless G_DEBUG_ROWS;
-    say "$char ($result)";
-}
 
 sub get_cached($key)
 {
     $cache_attempts++;
     if (exists $cache{$key}) {
         $cache_hits++;
+        $cache_lru{$key} = $cache_attempts;
         return $cache{$key};
     }
 
@@ -110,107 +62,60 @@ sub set_cached($key, $value)
 {
     $cache_inserts++;
     $cache{$key} = $value;
+    $cache_lru{$key} = $cache_attempts;
+    if (%cache > 6000) {
+        my @sorted_keys =
+            map { $_->[0] }
+            sort { $a->[1] <=> $b->[1] }
+            map { [ $_, $cache_lru{$_} ]}
+            keys %cache_lru;
+        splice(@sorted_keys, 1000); # remove 1000 old entries
+        delete @cache{@sorted_keys};
+        delete @cache_lru{@sorted_keys};
+    }
     return $value;
 }
 
 # counts all cases starting at template until there is no more room
-sub count_sub_case($partlen, $template, $level, @rest)
+sub count_sub_case($template, $partlen, @rest)
 {
     my $key = join(',', $template, $partlen, @rest);
-
-    ll($level, "[$partlen;@rest]: $template");
 
     my $result = get_cached($key);
     return $result if defined $result;
 
-    if (!$template and !@rest) {
-        fi("none", 1);
-        return set_cached($key, 1);
-    }
-
-    unless ($template) {
-        fi("still input", 0);
-        return set_cached($key, 0);
-    }
-
-    if ($partlen && !$template) {
-        fi("still parts", 0);
-        return set_cached($key, 0);
-    }
-
-    # last subsegment doesn't need '.' but does need to end the string
-    my $re_str = ('[#?]') x $partlen;
-    $re_str .= '[.?]' if @rest; # ensure part actually ends
-
-    my $re = qr/^$re_str/;
+    $template =~ s/^[.]*//; # skip leading '.'
+    return set_cached($key, 0) unless $template;
 
     # be strict on cases. we look at one char and that's it...
-    # this reads but does not consume input from template
-    my $cur_char = substr ($template, 0, 1);
-
-    while ($cur_char eq '.') {
-        # skip parts of template that can't consume input
-
-        # consume input we peeked at earlier
-        substr ($template, 0, 1, '');
-
-        $cur_char = substr ($template, 0, 1);
-
-        # if no template to consume input against, finish up
-        unless ($template) {
-            fi("ran out of input skipping empty", 0);
-            return set_cached($key, 0);
-        }
-    }
+    my $cur_char = substr ($template, 0, 1, ''); # changes $template
 
     # We have an input to match
     if ($cur_char eq '#') {
+        # last subsegment doesn't need '.'
+        my $re_str = '^' . ('[#?]') x ($partlen - 1) . ('[.?]') x (@rest ? 1 : 0);
+
         # if we can't match here, we're done
-        unless ($template =~ $re) {
-            fi("match needed on $template but failed", 0);
-            return set_cached($key, 0);
-        }
+        return set_cached($key, 0) unless $template =~ /$re_str/;
 
-        # we've matched, consume the input
-        $template = substr ($template, $partlen + (@rest ? 1 : 0));
+        # we've matched, consume rest of the input
+        $template = substr ($template, $partlen - 1 + (@rest ? 1 : 0));
 
-        # if we *can* match here, see if there's later parts or input.
-        if (!@rest && $template =~ /^[.?]*$/) {
-            fi("more filler input to consume, no more parts", 1);
-            return set_cached($key, 1);
-        }
-        if (!@rest && $template) {
-            fi("more substantive input to consume, but no more parts", 0);
-            return set_cached($key, 0);
-        }
-        if (@rest && !$template) {
-            fi("more parts but no more input", 0);
-            return set_cached($key, 0);
-        }
+        # if there's nothing left to match figure it out before recursion
+        return set_cached($key, 0) if !@rest and $template =~ /#/;
+        return set_cached($key, 1) if !@rest and $template =~ /^[.?]*$/;
+        return set_cached($key, 0) if  @rest and $template =~ /^\.*$/;
 
-        # potential for this to be a match, if we match later as well
-        my ($next_part, @next_rest) = @rest;
-
-        fi ("prospective match, delegating", 1);
-        my $count = count_sub_case($next_part, $template, $level + 1, @next_rest);
-
-        ll($level, "[$partlen;@rest]: $template");
-        fi ("completed match, result", $count);
-
-        return set_cached($key, $count);
+        # special cases gone, need to keep looking. Note deliberate missed
+        # second param in recursion to destructure partnet in nested call
+        return set_cached($key, count_sub_case($template, @rest));
     } elsif ($cur_char eq '?') {
-        # combine both paths by calling cur path again using both possibilities
-        my $templ_filled = $template;
-        my $templ_empty = $template;
-        substr($templ_filled, 0, 1) = '#'; # replace '?' with '#'
-        substr($templ_empty, 0, 1) = '.'; # replace '?' with '.'
+        # combine both paths (# or .) by calling cur path again using both
+        # possibilities. with . we just keep going rather than add it in
 
-        fi ("wildcard, delegating", 1);
-        my $count = count_sub_case($partlen, $templ_filled, $level + 1, @rest)
-            + count_sub_case($partlen, $templ_empty, $level + 1, @rest);
-
-        ll($level, "[$partlen;@rest]: $template");
-        fi ("completed wildcard", $count);
+        my $count =
+            count_sub_case("#$template", $partlen, @rest) +
+            count_sub_case(  $template , $partlen, @rest);
 
         return set_cached($key, $count);
     } else {
@@ -221,6 +126,5 @@ sub count_sub_case($partlen, $template, $level, @rest)
 
 sub count_valid_rows($template, @partlens)
 {
-    my ($part, @rest) = @partlens;
-    return count_sub_case($part, $template, 0, @rest);
+    return count_sub_case($template, @partlens);
 }
