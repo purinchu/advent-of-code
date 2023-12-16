45e920ab9a203bce4a4bc3b87f32b6bf493d93c3
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2023-12-16 11:32:26 -0500

    p32: Try to optimize path tracing for bigger part 2.
    I suspect they want us to do memoization or dynamic programming again
    (e.g. map 'x/y/dir' to 'all line segments impacted') but this might not
    be so bad with brute force if I optimize.

---
+++ 2023/32/beam.pl
@@ -0,0 +1,202 @@
+#!/usr/bin/env perl
+
+# AoC 2023 - Puzzle 31 (Day 16 Part 1)
+# This problem requires to read in an input file that ultimately
+# lists information about info grids to find where these grids have
+# beams cross over them
+# See the Advent of Code website.
+
+use 5.038;
+use autodie;
+
+use List::Util qw(sum first zip min max);
+
+# Config
+use constant G_DEBUG_INPUT => 0;
+use constant G_DEBUG_ANIMATE => 0;
+use constant G_DEBUG_OUTPUT => 0;
+
+my $input_name = '../31/input';
+$" = ', '; # For arrays interpolated into strings
+
+# Globals
+
+my @grids; # Mirror grid
+my @tiles; # Portions of grid energized
+my @beams; # Beams of light in motion
+my %visited_tiles; # Detects cycles of beams
+my ($h, $w); # Grid dimension
+
+# Code (Aux subs below)
+
+do {
+    open my $input_fh, '<', (shift @ARGV // $input_name);
+    local $/ = ""; # go into 'paragraph mode'
+    chomp(my @paragraphs = <$input_fh>);
+    load_input(@paragraphs);
+};
+
+if (G_DEBUG_INPUT) {
+    say join('', $_->@*) foreach @grids;
+    say "";
+    say join('', $_->@*) foreach @tiles;
+    say "";
+}
+
+# Handle beams
+push @beams, ['E', 0, 0]; # dir, x, y, ttl
+
+if (G_DEBUG_ANIMATE) {
+    # use alternate terminal screen if we want to show in situ progress
+    system ('tput', 'smcup');
+    $SIG{INT} = sub { system('tput', 'rmcup'); exit 1; };
+}
+
+while (@beams && @beams < 2048) {
+    my ($dir, $x, $y) = (shift @beams)->@*;
+
+    visit($x, $y, $dir);
+
+    if (G_DEBUG_ANIMATE){
+        draw_screen_overlayed();
+        sleep 1;
+    }
+
+    add_next_moves($x, $y, $dir);
+}
+
+system ('tput', 'rmcup') if G_DEBUG_ANIMATE;
+dump_screen_with_color() if G_DEBUG_OUTPUT;
+
+say scalar grep { $_ eq '#' } map { ($_->@*) } @tiles;
+
+say "Likely died due to lotsa beams" if @beams >= 2048;
+
+# Aux subs
+
+sub load_input(@paras)
+{
+    # should be only one para for this exercise
+    for (@paras) {
+        s/\n*$//;
+        # break into lines then chars
+        @grids = map { [split('')] } split("\n");
+    }
+
+    @tiles = map { [('.') x (@$_)] } @grids;
+    $h = @grids;
+    $w = $grids[0]->@*;
+}
+
+sub dump_screen_with_color
+{
+    for my $y (0..($h-1)) {
+        for my $x (0..($w-1)) {
+            my $c = $tiles[$y]->[$x] // '';
+            if ($c eq '#') {
+                # energized
+                print "\e[1;31;46m"; # cyan bg, bright red fg
+            } else {
+                print "\e[37;49m"; # default bg, white fg
+            }
+            print $grids[$y]->[$x];
+        }
+        print "\e[37;49m"; # default bg, white fg
+        say "";
+    }
+}
+
+sub draw_screen_overlayed
+{
+    # clear screen and reset cursor
+    print "\e[H\e[2J";
+
+    print "\e[31m"; # color
+    say join('', $_->@*) foreach @grids;
+    say "";
+
+    # save cursor position then back up over lines we just draw, and reset
+    # color
+    my $up = $h + 1;
+    print ("\e[7\e[${up}F\e[0m");
+
+    for my $y (0..$h) {
+        for my $x (0..$w) {
+            my $c = $tiles[$y]->[$x] // '';
+            if ($c eq '#') {
+                print $tiles[$y]->[$x];
+            } else {
+                print "\e[1C"; # skip column
+            }
+            select()->flush();
+        }
+        print "\e[1E"; # down 1, back to start
+    }
+
+    # restore cursor position
+    print ("\e[8");
+}
+
+sub visit ($x, $y, $dir)
+{
+    $tiles[$y]->[$x] = '#';
+
+    # need separate cache because multiple beams can appear going different
+    # directions. We can only skip if direction was also seen
+    $visited_tiles{"$x/$y/$dir"} = 1;
+}
+
+sub add_next_moves ($x, $y, $indir)
+{
+    state %newdir = (
+        '/E' => 'N', '/W' => 'S', '/N' => 'E', '/S' => 'W',
+        '\E' => 'S', '\W' => 'N', '\N' => 'W', '\S' => 'E',
+    );
+
+    my $tile = $grids[$y]->[$x];
+    my $horiz = ($indir eq 'E' or $indir eq 'W');
+
+    if ($horiz && ($tile eq '.' || $tile eq '-')) {
+        my $row = $grids[$y];
+
+        if ($indir eq 'E') {
+            my $end = ((first { $row->[$_] ne '-' and $row->[$_] ne '.' } $x..($w-1)) // $w) - 1;
+            visit($_, $y, $indir) foreach $x..$end;
+            add_beam($end, $y, $indir) if $end < ($w - 1); # only add beam if we hit a mirror
+        } else {
+            my $end = ((first { $row->[$_] ne '-' and $row->[$_] ne '.' } reverse 0..$x) // 0) + 1;
+            visit($_, $y, $indir) foreach $end..$x;
+            add_beam($end, $y, $indir) if $end > 0; # only add beam if we hit a mirror
+        }
+    }
+    elsif (!$horiz && ($tile eq '.' || $tile eq '|')) {
+        add_beam($x, $y, $indir);
+    }
+    elsif ($tile eq '|' && $horiz) {
+        add_beam($x, $y, 'N');
+        add_beam($x, $y, 'S');
+    }
+    elsif ($tile eq '-' && !$horiz) {
+        add_beam($x, $y, 'E');
+        add_beam($x, $y, 'W');
+    }
+    else {
+        add_beam($x, $y, $newdir{"$tile$indir"});
+    }
+}
+
+# starting from sx,sy, moves in dir and, if valid and not seen, adds a
+# beam for later processing
+sub add_beam ($sx, $sy, $dir)
+{
+    state %xoff = (E => 1, W => -1);
+    state %yoff = (S => 1, N => -1);
+
+    my $x = $sx + ($xoff{$dir} // 0);
+    my $y = $sy + ($yoff{$dir} // 0);
+
+    return unless ($x >= 0 && $x < $w && $y >= 0 && $y < $h);
+    return if exists $visited_tiles{"$x/$y/$dir"};
+
+    push @beams, [$dir, $x, $y];
+};
