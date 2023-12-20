3953e493b8585c23c6e6ac7a19bab45e15623fa3
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2023-12-20 13:10:48 -0500

    p40: Not ready yet but getting somewhere.

--- 2023/40/pulse.pl
+++ 2023/40/pulse.pl
@@ -16,6 +16,7 @@ use Getopt::Long qw(:config auto_version auto_help);
 
 # Config
 use constant G_DEBUG_INPUT => 0;
+use constant G_DEBUG_LINES => 1;
 use constant G_DEBUG_EV => 0;
 
 my $input_name = 'input';
@@ -23,17 +24,20 @@ $" = ', '; # For arrays interpolated into strings
 
 my %modules; # broadcaster, button, flip-flop, inverters, conjunctors
 my @events;  # event loop
+my $button_press = 0; # number of times button was pushed
 
 # Code (Aux subs below)
 
 my $max_cycles = 1000; # stop after this many cycles
 my $watched;           # node name to watch
 my $spam_msgs;         # output every watched message in/out
+my $propagate;         # try to determine cycle period by propagating push
 
 GetOptions(
     "cycles|c=i"   => \$max_cycles,
     "watch|w=s"    => \$watched,
     "spam|s"       => \$spam_msgs,
+    "propagate|p"  => \$propagate,
 )
     or die "Error reading command line options";
 
@@ -41,6 +45,9 @@ do {
     open my $input_fh, '<', (shift @ARGV // $input_name);
     chomp(my @lines = <$input_fh>);
 
+    if (G_DEBUG_LINES) {
+        say for @lines;
+    }
     %modules = load_input(@lines);
 };
 
@@ -56,17 +63,34 @@ if (G_DEBUG_INPUT) {
 my $count = 0;
 my @tally = (0, 0);
 
-# push button repeatedly
-for (1..$max_cycles) {
-    push @events, make_event('broadcaster', 'button', 0);
-    while (@events) {
-        $tally[handle_event(\@events)]++;
+if ($propagate) {
+    say "Propagating low pulse periods";
+
+    my ($new_p, @next_updates) = propagate_first_low_pulse(1, 'broadcaster');
+    while (@next_updates) {
+        my $node = shift @next_updates;
+        my @more;
+
+        ($new_p, @more) = propagate_first_low_pulse($new_p, $node);
+        push @next_updates, @more;
     }
+
+    say "Done!";
+} else {
+    # push button repeatedly
+    for (1..$max_cycles) {
+        push @events, make_event('broadcaster', 'button', 0);
+        while (@events) {
+            $tally[handle_event(\@events)]++;
+        }
+
+        $button_press++;
+    }
+
+    say $tally[0] * $tally[1];
 }
 
-if (!$watched) {
-    say $tally[0] * $tally[1];
-} else {
+if ($watched) {
     my $json = JSON->new->pretty;
     say $json->encode($modules{$watched});
 }
@@ -118,6 +142,87 @@ sub load_input(@lines)
     return %modules;
 }
 
+sub lcm($a, $b)
+{
+    # factorize both and use the highest number of each multiple seen
+    my %highest_factors;
+
+    say "Getting lcm of $a, $b";
+
+    for my $val ($a, $b) {
+        my %cur_factors;
+        my $highest = sqrt($val) + 1;
+        my $divisor = 2;
+
+        while($val > $divisor && $divisor < $highest) {
+            if (($val % $divisor) == 0) {
+                # found a multiple
+                $cur_factors{$divisor}++;
+                $val /= $divisor;
+            } else {
+                $divisor++;
+            }
+        }
+
+        $cur_factors{$val}++; # Last multiplication factor
+
+        for my ($div, $count) (%cur_factors) {
+            $highest_factors{$div} = max($count, $highest_factors{$div} // 1);
+        }
+    }
+
+    my $result = 1;
+    for my ($div, $count) (%highest_factors) {
+        $result *= ($div * $count);
+    }
+
+    say "lcm of $a, $b is $result";
+
+    return $result;
+}
+
+# returns list of nodes that must be updated due to this change,
+# first_press is the number of button presses until the **0** pulse!
+sub propagate_first_low_pulse($first_press, $node)
+{
+    my $m = $modules{$node};
+
+    say "Propagating $node";
+
+    if (!exists $m->{in_cycle_0}) {
+        # our propagation varies by type
+        if ($m->{type} eq 'b') {
+            # broadcast, every push is a 0
+            $first_press = 0;
+        } elsif ($m->{type} eq '%') {
+            # flip flop, every other input 0 is a 0 out
+            $first_press = $first_press + 1;
+        } elsif ($m->{type} eq '&') {
+            # conjunction, every node being a 1 gives us a 0 out
+            # assume the math works out for now...
+            my @in_periods =
+                map { $modules{$_}->{in_cycle_0} // 1 }
+                keys $m->{in}->%*;
+            my $lcm = reduce { lcm ($a, $b) } $first_press, @in_periods;
+            $first_press = $lcm;
+        } else {
+            say "Unhandled node $node, setting first_press to $first_press";
+        }
+
+        $m->{in_cycle_0} = $first_press;
+    } else {
+        my $old_cycle = $m->{in_cycle_0} // 1;
+        $first_press = lcm($first_press, $old_cycle);
+        $m->{in_cycle_0} = $first_press;
+        return ($first_press) unless $old_cycle != $first_press;
+    }
+
+    say "Done with $node, updating $m->{out}->@*";
+
+    # our first_press changed, propagate changes to our receivers
+    return ($first_press, $m->{out}->@*);
+}
+
 sub make_event($recv, $src, $pulse)
 {
     { r => $recv, src => $src, t => $pulse };
@@ -145,6 +250,8 @@ sub handle_event($events_ref)
         say "$event_num: $watched: received $e->{t} from $e->{src}"
             if $spam_msgs;
 
+        $r->{"first_$p"} //= $button_press;
+
         # see if a periodic cycle is there
         my $last_ev = $r->{"last_$p"} // 0;
         if ($last_ev) {
@@ -154,9 +261,7 @@ sub handle_event($events_ref)
             $r->{"cycle_$p"} = $this_cycle;
 
             if ($last_cycle && $last_cycle != $this_cycle) {
-                say "$event_num: $watched: cycle $p went from $last_cycle to $this_cycle!";
-            } elsif (!$last_cycle) {
-                say "$event_num: $watched: detected cycle of length $this_cycle for $p.";
+                $r->{"aperiodic_cycle_${p}_detected"} = 1;
             }
         }
 
@@ -218,6 +323,7 @@ Usage: ./pulse.pl [-c NUM] [-s] [-w NODE_NAME] [FILE_NAME]
                        the number of cycles between receiving low pulses is
                        output.
   -s | --spam       -> When watching a node, also output msgs sent/received.
+  -p | --propagate  -> Try to determine common cycle in network, use with -w
 
 FILE_NAME specifies the network configuration to simulate, and is 'input'
 if not specified.
