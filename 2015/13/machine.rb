d83333feb4e7a156dd1451170552db05725da60b
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2023-12-20 22:41:50 -0500

    2015/13: Took longer than I want to admit, but done.
    The duplicate instances for the same var were impossible to reconcile
    until I remembers that's why I had a single Puzzle class... added a
    table of vars to it for Variable nodes to reach back into and it was
    done.

---
+++ 2015/13/machine.rb
@@ -0,0 +1,194 @@
+#!/usr/bin/env ruby
+
+# 2015 Day 7 Part 1 / Puzzle 2015/13
+
+class Puzzle
+  include Enumerable
+
+  def initialize()
+    @symtab = {}
+    @values = {}
+    @ready  = {} # outputs which can be read
+  end
+
+  def assignOutput(result, expression)
+    @symtab[result] = expression
+    @ready[result] = expression.ready?
+  end
+
+  def lookup(name)
+    return @symtab[name]
+  end
+
+  def setVar(name, result)
+    @values[name] = result
+  end
+
+  def var?(name)
+    return @values.key?(name)
+  end
+
+  def each(&block)
+    @symtab.each(&block)
+  end
+end
+
+class Op
+  def initialize(p)
+    @p = p
+    @ready = false
+    @value = nil
+  end
+
+  def ready?
+    @ready
+  end
+
+  def evaluate
+    if @value.nil?
+      @value = evaluate_impl
+    end
+    return @value
+  end
+end
+
+class BinOp < Op
+  def initialize(p, op, l, r)
+    super(p)
+    @l = l
+    @r = r
+    @ready = l.ready? and r.ready?
+
+    case op
+    when "LSHIFT"
+      @op = ->(l, r) { (l << r) & 0xFFFF }
+    when "RSHIFT"
+      @op = ->(l, r) { (l >> r) & 0xFFFF }
+    when "AND"
+      @op = ->(l, r) { (l & r) & 0xFFFF }
+    when "OR"
+      @op = ->(l, r) { (l | r) & 0xFFFF }
+    else
+      raise "Unhandled operation #{op}"
+    end
+  end
+
+  def to_s
+    return "#{@l} OP #{@r}"
+  end
+
+  def evaluate_impl
+    val_l = @l.evaluate().to_i
+    val_r = @r.evaluate().to_i
+    return @op.(val_l, val_r)
+  end
+end
+
+class UnaryOp < Op
+  def initialize(p, op, operand)
+    super(p)
+    @operand = operand
+    @ready = operand.ready?
+
+    case op
+    when "NOT"
+      @op = ->(val) { (~val) & 0xFFFF }
+    else
+      raise "Unhandled operation #{op}"
+    end
+  end
+
+  def to_s
+    return "-(#{@op})"
+  end
+
+  def evaluate_impl
+    return @op.(@operand.evaluate())
+  end
+end
+
+class Var < Op
+  def initialize(p, op)
+    super(p)
+    @op = op
+  end
+
+  def ready?
+    return (@ready or @p.var?(@op))
+  end
+
+  def to_s
+    return "#{@op}"
+  end
+
+  def evaluate_impl
+    res = (@p.lookup(@op).evaluate()) & 0xFFFF
+    @p.setVar(@op, res)
+    @ready = true
+    return res
+  end
+end
+
+class Literal < Op
+  def initialize(p, op)
+    super(p)
+
+    @op = Integer(op)
+    @ready = true
+  end
+
+  def to_s
+    return "#{@op}"
+  end
+
+  def evaluate_impl
+    return @op
+  end
+end
+
+def decode(line, p)
+  # update state of puzzle p
+  input, output = line.split(' -> ')
+
+  result = nil
+  ready = false
+
+  read_lit = ->(term) {
+    a = begin Literal.new(p, term) rescue Var.new(p, term) end
+    return a
+  }
+
+  # input can be a literal number, binary operator, or unary op try them bin,
+  # unary, literal. Make the output depend on the input being ready to manage
+  # the flow graph.
+  case input
+  when /^(\w+) (AND|OR|LSHIFT|RSHIFT) (\w+)$/
+    # binary op
+    l = read_lit.($1)
+    r = read_lit.($3)
+    result = BinOp.new(p, $2, l, r)
+  when /^NOT (\w+)$/
+    # unary op
+    operand = read_lit.($1)
+    result = UnaryOp.new(p, 'NOT', operand)
+  when /^(\w+)$/
+    # literal. yes the input has vars too not just constants
+    result = read_lit.($1)
+  else
+    raise "Can't understand #{line}"
+  end
+
+  p.assignOutput(output, result)
+end
+
+p = Puzzle.new()
+
+File.foreach(ARGV.shift || 'input') { |line|
+  decode(line.chomp, p)
+}
+
+p.each { |sym, expr|
+  puts "#{sym} -> #{expr.evaluate}"
+}
+
+#puts p.lookup('a').evaluate
