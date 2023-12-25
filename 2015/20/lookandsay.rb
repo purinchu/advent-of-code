4de81a28bddca4629f137aca10a2640eb180ec07
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2023-12-24 20:51:01 -0500

    2015/20: Same exact solution for Day 10 part 2.
    I was initially concerned trying to run this to 50 steps would kill the
    computer but turns out the growth is mostly polynomial in input size.

---
+++ 2015/20/lookandsay.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+# 2015 Day 10 Part 2 / Puzzle 2015/20
+
+class Puzzle
+  def initialize()
+    @digits = []
+  end
+
+  def decode(line)
+    @digits = line.split('').map &:to_i
+  end
+
+  def run_step
+    new_a = @digits.chunk_while { |l,r| l==r }.to_a
+    new_digits = new_a.map { |arr|
+      [arr.length, arr[0]].join('')
+    }
+    decode(new_digits.join(''))
+  end
+
+  def to_s
+    @digits.join('')
+  end
+
+  def digits
+    @digits
+  end
+end
+
+p = Puzzle.new()
+
+File.foreach(ARGV.shift || 'input') { |line|
+  p.decode(line.chomp)
+}
+
+50.times { |i|
+  p.run_step
+  puts "step #{i}"
+}
+
+puts p.digits.length
