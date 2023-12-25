490579fed90a1e297dcc875f89db6fd454a714e9
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2023-12-24 21:23:15 -0500

    2015/23: Commit working solution for day 12 part 1.

---
+++ 2015/23/json.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+# 2015 Day 12 Part 1 / Puzzle 2015/23
+
+require 'json'
+
+class Puzzle
+  def initialize()
+  end
+
+  def decode(line)
+    @data = JSON.parse(line)
+  end
+
+  def sum_hash(h)
+    total = 0
+    h.each_pair {|_,v|
+      case v
+      when Integer
+        total += v
+      when Array
+        total += sum_array(v)
+      when Hash
+        total += sum_hash(v)
+      end
+    }
+
+    total
+  end
+
+  def sum_array(a)
+    total = 0
+    a.each { |v|
+      case v
+      when Integer
+        total += v
+      when Array
+        total += sum_array(v)
+      when Hash
+        total += sum_hash(v)
+      end
+    }
+    total
+  end
+
+  def sum
+    sum_hash(@data)
+  end
+end
+
+p = Puzzle.new()
+
+File.foreach(ARGV.shift || 'input') { |line|
+  p.decode(line.chomp)
+}
+
+puts p.sum
