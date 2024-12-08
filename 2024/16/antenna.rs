59fbc5412ae3dcef35f1346bd5c54695d6a1f03b
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2024-12-08 11:03:35 -0500

    2024/16: More simplifications.
    It's possible to use some Rust-style iterators instead of a for loop to
    generate the antipodes, which simplifies things significantly even at
    the cost of a couple of aux functions.
    
    With that change we don't need the hard-coded combination function, the
    loops are easy to generate inline and now that the core loop is so small
    it doesn't distract in the code to do this.

--- 2024/16/antenna.rs
+++ 2024/16/antenna.rs
@@ -16,36 +16,6 @@ fn is_in_bounds(i: usize, j: usize, w: usize, h: usize) -> bool {
     return i < w && j < h;
 }
 
-// I couldn't think of any fancy algorithm for this and I'm not using crates.  Luckily my input
-// liked me!  Only 3- and 4-sized groups in the input along with the sample.
-fn combinations(n: usize, r: usize) -> Vec<Vec<usize>>
-{
-    let combos_3_2: Vec<Vec<usize>> = vec![
-        vec![0, 1],
-        vec![0, 2],
-        vec![1, 2],
-    ];
-
-    let combos_4_2: Vec<Vec<usize>> = vec![
-        vec![0, 1],
-        vec![0, 2],
-        vec![0, 3],
-        vec![1, 2],
-        vec![1, 3],
-        vec![2, 3],
-    ];
-
-    if n == 3 && r == 2 {
-        return combos_3_2;
-    }
-
-    if n == 4 && r == 2 {
-        return combos_4_2;
-    }
-
-    panic!("Unimplemented");
-}
-
 fn find_antennae(lines: Vec<String>) -> Vec<Antenna>
 {
     let mut antenna: Vec<Antenna> = Vec::with_capacity(64);
@@ -59,6 +29,13 @@ fn find_antennae(lines: Vec<String>) -> Vec<Antenna>
     };
 
     return antenna;
+}
+
+fn project_out(pt: (usize, usize), d: (i32, i32), count: usize) -> (usize, usize)
+{
+    let (x, y) = pt;
+    let (dx, dy) = d;
+    return ((x as i32 + dx * count as i32) as usize, (y as i32 + dy * count as i32) as usize);
 }
 
 fn main() {
@@ -87,49 +64,30 @@ fn main() {
 
     for grp in &ant_groups {
         let ants = &antenna.iter().filter(|x| x.group == *grp).collect::<Vec<_>>();
-        let n = ants.len();
 
-        for combo in combinations(n, 2) {
-            let ant_l = ants[combo[0]];
-            let ant_r = ants[combo[1]];
+        for i in 0..ants.len() {
+            for j in (i+1)..ants.len() {
+                let (ant_l, ant_r) = (ants[i], ants[j]);
 
-            // Every antenna is also an antipode. Dups will be handled later.
-            antipode_pos.push((ant_l.x, ant_l.y));
-            antipode_pos.push((ant_r.x, ant_r.y));
+                let dx = ant_l.x as i32 - ant_r.x as i32;
+                let dy = ant_l.y as i32 - ant_r.y as i32;
 
-            let dx: i32 = ant_l.x as i32 - ant_r.x as i32;
-            let dy: i32 = ant_l.y as i32 - ant_r.y as i32;
+                // Start from 0 because every antenna is also an antipode.
+                antipode_pos.extend((0..)
+                    .map(|i| project_out((ant_l.x, ant_l.y), (dx, dy), i))
+                    .take_while(|(x,y)| is_in_bounds(*x, *y, w, h))
+                );
 
-            let mut count = 1;
-            loop {
-                let anti_x = ant_l.x as i32 + dx * count;
-                let anti_y = ant_l.y as i32 + dy * count;
-
-                if is_in_bounds(anti_x as usize, anti_y as usize, w, h) {
-                    antipode_pos.push((anti_x as usize, anti_y as usize));
-                    count += 1;
-                } else {
-                    break;
-                }
-            }
-
-            count = 1;
-            loop {
-                let anti_x = ant_r.x as i32 - dx * count;
-                let anti_y = ant_r.y as i32 - dy * count;
-
-                if is_in_bounds(anti_x as usize, anti_y as usize, w, h) {
-                    antipode_pos.push((anti_x as usize, anti_y as usize));
-                    count += 1;
-                } else {
-                    break;
-                }
+                antipode_pos.extend((0..)
+                    .map(|i| project_out((ant_r.x, ant_r.y), (-dx, -dy), i))
+                    .take_while(|(x,y)| is_in_bounds(*x, *y, w, h))
+                );
             }
         }
     }
 
     antipode_pos.sort();
-    antipode_pos.dedup();
+    antipode_pos.dedup(); // multiple groups may have antipode in same spot
 
     println!("{}", antipode_pos.len());
 }
