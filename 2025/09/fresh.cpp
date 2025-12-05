abba049b3fb415efd85e0d208ab6bee2bf29b170
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2025-12-05 18:42:10 -0500

    2025/09: Working solution for day 5 part 1.
    I don't *like* the solution, but the #if 0 part was off by 14.
    
    I know which 14 elements were different so I'll go back to see about why
    the solution had trouble a bit later, hopefully it's some simple
    oversight.

---
+++ 2025/09/fresh.cpp
@@ -0,0 +1,138 @@
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+using std::array;
+using std::make_pair;
+using std::pair;
+using std::size_t;
+using std::string;
+using std::string_view;
+using std::vector;
+
+namespace stdr = std::ranges;
+namespace stdv = std::views;
+
+using namespace std::literals::string_view_literals;
+
+using ID          = std::uint64_t;
+using FreshRange  = std::pair<ID, ID>;
+using FreshIDs    = vector<FreshRange>;
+using Ingredients = vector<ID>;
+using Data        = std::pair<FreshIDs, Ingredients>;
+
+static auto bounds_from_range(std::string_view freshness)
+    -> pair<ID, ID>
+{
+    size_t pos = freshness.find("-"sv);
+
+    const auto left = freshness.substr(0, pos);
+    const auto right = freshness.substr(pos + 1);
+
+    ID outl, outr;
+    std::from_chars(left.data(), left.data() + left.size(), outl);
+    std::from_chars(right.data(), right.data() + right.size(), outr);
+
+    return std::make_pair(outl, outr);
+}
+
+static Data get_freshness_input(const string &fname)
+{
+    FreshIDs ids;
+    Ingredients ingrs;
+
+    std::ifstream in_f(fname, std::ios::in);
+    if (!in_f.is_open()) {
+        throw std::runtime_error("Failed to open file");
+    }
+
+    string out;
+    while(std::getline(in_f, out) && !out.empty()) {
+        ids.emplace_back(bounds_from_range(out));
+    }
+
+    stdr::sort(ids);
+
+    while(std::getline(in_f, out)) {
+        ID out_id;
+        std::from_chars(out.data(), out.data() + out.size(), out_id);
+        ingrs.emplace_back(out_id);
+    }
+
+    stdr::sort(ingrs);
+
+    return make_pair(std::move(ids), std::move(ingrs));
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        std::cerr << "Pass filename to read\n";
+        return 1;
+    }
+
+    const string fname(argv[1]);
+    try {
+        const auto [freshness, ingredients] = get_freshness_input(fname);
+
+        auto &&lower = freshness.begin();
+        const auto &upper = freshness.end();
+        int count = 0;
+
+        for (const auto &i : ingredients) {
+            // anything in this range or after cannot match i
+            const auto ceil = std::upper_bound(lower, upper, i, [](const ID val, const FreshRange &fr) {
+                const auto &[l, r] = fr;
+                return val < l;
+            });
+            if (ceil == lower) {
+                // nothing can match, at least based on lower bound
+                continue;
+            }
+
+            bool match = false;
+            for (auto it = lower; it != ceil; ++it) {
+                const auto &[l, r] = *it;
+                if (i >= l && i <= r) {
+                    match = true;
+                    count++;
+                    break;
+                }
+            }
+            if (match) {
+                continue;
+            }
+#if 0
+
+            // only things in this range or after can match i
+            const auto start = std::lower_bound(lower, ceil, i, [](const FreshRange &fr, const ID val) {
+                const auto &[l, r] = fr;
+                return r < val;
+            });
+            if (start == ceil) {
+                continue;
+            }
+
+            count++;
+#endif
+        }
+
+        std::cout << count << "\n";
+    }
+    catch (std::runtime_error &err) {
+        std::cerr << "Error " << err.what() << " while handling " << fname << "\n";
+        return 1;
+    }
+    catch (...) {
+        std::cerr << "Unknown error handling " << fname << "\n";
+        return 1;
+    }
+
+    return 0;
+}
