83b2e837744a6496a8fe5b712547d239dac40fca
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2025-12-04 23:23:59 -0500

    2025/08: Implement a SWAR approach.
    I would have added "for a speedup", but this didn't actually change the
    speed very much if at all, even with an 8-byte stride.  But it was fun
    to get to think about how to do it correctly so I'm holding onto the
    work I did.

--- 2025/08/rolls.cpp
+++ 2025/08/rolls.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <array>
+#include <cstring>
 #include <charconv>
 #include <iostream>
 #include <fstream>
@@ -15,6 +16,7 @@ using std::tuple;
 using std::cout;
 using std::cerr;
 using std::string;
+using std::make_pair;
 using std::vector;
 using std::size_t;
 using std::string_view;
@@ -67,19 +69,16 @@ static void find_rolls_free(const Grid &g, vector<uint8_t> &surrounding_count)
         cout << "---\n\n";
     };
 
-    for (size_t i = 0; i < g.chars.size(); i++) {
-        if (g.chars[i] != '@') {
-            continue;
-        }
-        size_t c = i % g.w;
-        size_t r = i / g.w;
+    static const auto ring = array { // col, row
+        make_pair(-1, -1), make_pair( 0, -1), make_pair( 1, -1),
+        make_pair(-1,  0),                    make_pair( 1,  0),
+        make_pair(-1,  1), make_pair( 0,  1), make_pair( 1,  1),
+    };
 
-        using std::make_pair;
-        const auto ring = array { // col, row
-            make_pair(-1, -1), make_pair( 0, -1), make_pair( 1, -1),
-            make_pair(-1,  0),                    make_pair( 1,  0),
-            make_pair(-1,  1), make_pair( 0,  1), make_pair( 1,  1),
-        };
+    const auto update_xy = [&g, &surrounding_count](size_t c, size_t r) {
+        if (g.chars[r * g.w + c] != '@') {
+            return;
+        }
 
         for (const auto &[col, row] : ring) {
             const size_t newc = c + col;
@@ -88,6 +87,87 @@ static void find_rolls_free(const Grid &g, vector<uint8_t> &surrounding_count)
                 surrounding_count[g.w * newr + newc]++;
             }
         }
+    };
+
+    static constexpr const int STRIDE = 8;
+    using StrideInt = uint64_t;
+    const auto swar_at = [&g, &surrounding_count](size_t c, size_t r) {
+        static_assert(sizeof(StrideInt) == STRIDE);
+        StrideInt val;
+
+        const size_t idx = r * g.w + c;
+        std::memcpy(&val, &g.chars[idx], sizeof(val));
+        static constexpr const StrideInt mask = ~StrideInt(0) / 255 * '@'; // every byte has @
+        static constexpr const StrideInt ones = ~StrideInt(0) / 255 * 0x01;   // every byte has 0x01
+        static constexpr const StrideInt topb = ~StrideInt(0) / 255 * 0x80;   // every byte has 0x80
+
+        // See https://lemire.me/blog/2017/01/20/how-quickly-can-you-remove-spaces-from-a-string/
+        // for source of this function. What it does is for each of the four
+        // bytes that are zero, they are set to 0x80. The >>7 turns that to
+        // 0x01. We use xor to set zero bytes that match @
+        const StrideInt roll_p = val ^ mask; // bytes are 0 when @ present
+        const StrideInt haszero = (((roll_p) - ones) & ~roll_p & topb);
+        const StrideInt adder = haszero >> 7;
+        array<uint8_t, STRIDE> out;
+        array<uint8_t, STRIDE+2> conv_out {};
+        std::memcpy(out.data(), &adder, STRIDE);
+
+        // pre-process overlapped adds for rows above/below
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < STRIDE; j++) {
+                conv_out[i + j] += out[j];
+            }
+        }
+
+        // update row above
+        for (int j = 0; j < STRIDE + 2; j++) {
+            surrounding_count[idx - g.w - 1 + j] += conv_out[j];
+        }
+
+        // update this row (left and right)
+        for (int j = 0; j < STRIDE; j++) {
+            surrounding_count[idx - 1 + j] += out[j];
+            surrounding_count[idx + 1 + j] += out[j];
+        }
+
+        // update row below
+        for (int j = 0; j < STRIDE + 2; j++) {
+            surrounding_count[idx + g.w - 1 + j] += conv_out[j];
+        }
+    };
+
+    // Handle special-case individually but do most processing through SWAR
+    // methods
+
+    // first row
+    for (size_t c = 0; c < g.w; c++) {
+        update_xy(c, 0);
+    }
+
+    for (size_t r = 1; r < (g.h - 1); r++) {
+        update_xy(0, r);
+        size_t c = 1;
+
+        // meet alignment requirements
+        for ( ; c < STRIDE; c++) {
+            update_xy(c, r);
+        }
+
+        // c + STRIDE goes 1 too far which is why we don't need to check < g.w
+        // - 1 for the last column
+        while (c + STRIDE < g.w) {
+            swar_at(c, r);
+            c += STRIDE;
+        }
+
+        for ( ; c < g.w; c++) {
+            update_xy(c, r);
+        }
+    }
+
+    // last row
+    for (size_t c = 0; c < g.w; c++) {
+        update_xy(c, g.h - 1);
     }
 
     (void) dump_grid;
