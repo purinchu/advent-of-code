60c89453adbf01b070dd08d8603f62a6071d149e
Michael Pyne <30381288+purinchu@users.noreply.github.com>
2025-12-07 11:31:40 -0500

    2025/14: Minor tweaks and include part 1 calc.

--- 2025/14/tachyon.cpp
+++ 2025/14/tachyon.cpp
@@ -28,32 +28,31 @@ using Int = std::uint64_t;
 struct Grid
 {
     string chars;
-    size_t w;
-    size_t h;
+    size_t w = 0;
+    size_t h = 0;
 };
 
-// In case we SIMD later
-static constexpr const size_t STRIDE = 8;
-
-static Grid get_input_lines(const string &fname)
+static string inline file_slurp(const string &fname)
 {
-    Grid out;
-    std::ifstream in_f(fname, std::ios::in);
+    // ::ate is used to quickly get filesize with tellg
+    std::ifstream in_f(fname, std::ios::in | std::ios::ate);
     if (!in_f.is_open()) {
         throw std::runtime_error("Failed to open file");
     }
 
-    string buf;
+    const auto sz = in_f.tellg();
+    string str(sz, '\0');
+    in_f.seekg(0);
+    in_f.read(str.data(), str.size());
 
-    while (std::getline(in_f, buf)) {
-        const size_t des_sz = STRIDE * ((buf.size() + (STRIDE - 1)) / STRIDE);
-        buf.resize(des_sz, '.');
+    return str;
+}
 
-        out.chars.append(buf);
-
-        out.h++;
-        out.w = buf.size();
-    }
+static Grid get_input_lines(const string &fname)
+{
+    Grid out { file_slurp(fname) };
+    out.w = out.chars.find('\n');
+    out.h = stdr::count(out.chars, '\n');
 
     return out;
 }
@@ -66,37 +65,48 @@ int main(int argc, char *argv[])
     }
 
     string fname(argv[1]);
+    cout.sync_with_stdio(false);
 
     try {
         Grid g = get_input_lines(fname);
         cout << "Grid size: " << g.w << "," << g.h << "\n";
 
         vector<Int> tachyons(g.w, 0); // number of beams through a cell
+        bool start_found = false;
+        unsigned num_splits = 0;
 
-        for (size_t i = 0; i < g.h; i++) {
-            const string_view line(&g.chars[g.w * i], g.w);
+        for (const auto &subr : stdv::split(g.chars, '\n')) {
+            const string_view line(subr.begin(), subr.end());
 
             // look for start
-            const auto start_pos = line.find('S');
-            if (start_pos < line.size()) {
-                tachyons[start_pos] = 1;
-                continue;
+            if (!start_found) [[unlikely]] {
+                const auto start_pos = line.find('S');
+                if (start_pos < line.size()) {
+                    tachyons[start_pos] = 1;
+                    start_found = true;
+                    continue;
+                }
             }
 
             // look for splitters
-            auto &&splitters = line | stdv::enumerate
-                | stdv::filter([](const auto &tpl) { return get<1>(tpl) == '^'; });
+            auto &&splitters = line
+                | stdv::enumerate
+                | stdv::filter([](const auto &tpl) { return get<1>(tpl) == '^'; })
+                ;
+
             for (const auto [idx, ch] : splitters) {
-//              cout << "line " << i << " has a splitter at " << idx << "\n";
-                Int old = tachyons[idx];
+                const Int old = tachyons[idx];
                 tachyons[idx - 1] += old;
                 tachyons[idx + 1] += old;
-                tachyons[idx    ] =  0; // shielded by splitter
+                tachyons[idx    ]  = 0; // shielded by splitter
+                if (old > 0) {
+                    num_splits++;
+                }
             }
         }
 
         const Int total_sum = stdr::fold_left(tachyons, Int(0), std::plus{});
-        cout << total_sum << "\n";
+        cout << "sum=" << total_sum << ", splits=" << num_splits << "\n";
     }
     catch (std::runtime_error &err) {
         cerr << "Error " << err.what() << " while handling " << fname << "\n";
